9f5837949b2b535bf4a997bf945916cd3573b42f
vincent <vincent.bardou@etu-unistra.fr>
2023-03-16 13:11:47 +0100

    change movment method, add jump and anti double jump

--- GMD_project_code/Assets/scripts/PlayerController.cs
+++ GMD_project_code/Assets/scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float rotateSpeed = 1;
+    [SerializeField] private float jumpHeight = 1;
 
     private Rigidbody rg;
+    [SerializeField] private CollisionDetector groundDetector;
 
     [SerializeField] private float leftRightSpeed = 1;
     [SerializeField] private float forwardSpeed = 1;
@@ -19,23 +21,35 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //transform.Rotate(0, Input.GetAxis("Mouse X")*360*rotateSpeed*Time.deltaTime, 0);
-        Quaternion q = Quaternion.Euler(0, Input.GetAxis("Mouse X")*360*rotateSpeed*Time.deltaTime, 0);
-        rg.MoveRotation( transform.rotation*q);
+        //Quaternion q = Quaternion.Euler(0, Input.GetAxis("Mouse X")*360*rotateSpeed*Time.deltaTime, 0);
+        //rg.MoveRotation( transform.rotation*q);
+
+        Vector3 targetVelocity = new Vector3(0, 0, 0);
         
         float moveForward = Input.GetAxis("Forward");
-        Vector3 move = new Vector3(0, 0, 0);
+        //Vector3 move = new Vector3(0, 0, 0);
         if(moveForward != 0 )
         {
-            move += moveForward * forwardSpeed * transform.forward;
+            targetVelocity.z = moveForward * forwardSpeed;
+            //move += moveForward * forwardSpeed * transform.forward;
+            //rg.AddForce(moveForward * forwardSpeed  *Time.deltaTime * transform.forward);
+            //rg.velocity = moveForward * forwardSpeed * Time.deltaTime * transform.forward;
         }
         
         float moveRightLeft = Input.GetAxis("RightLeft");
         if (moveRightLeft != 0)
         {
-            move += moveRightLeft * leftRightSpeed * transform.right;
+            targetVelocity.x = moveRightLeft * forwardSpeed;
+            //rg.AddForce(moveRightLeft * leftRightSpeed *Time.deltaTime * transform.right);
         }
         
-        rg.MovePosition(transform.position + move * Time.deltaTime);
+        if (Input.GetButtonDown("Jump") && groundDetector.Grounded)
+        {
+            rg.AddForce(jumpHeight*transform.up, ForceMode.Impulse);
+        }
+
+        targetVelocity.y = rg.velocity.y;
+        rg.velocity = targetVelocity;
+        //rg.MovePosition(transform.position + move * Time.deltaTime);
     }
 }
