86ad0d03b1bdaa5fbf9d733d6359606f3f60c59c
Jeannot NAEGELY <jeannot.naegely@etu.unistra.fr>
2023-03-30 16:09:15 +0200

    Add modifications to FPS_movement_tuto scene (movement testing from a youtube tuto) + booster.cs modifications to try to make it work with the tuto version of the movement

--- GMD_project_code/Assets/scripts/Booster.cs
+++ GMD_project_code/Assets/scripts/Booster.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class Booster : MonoBehaviour
 {
+    private CharacterController Controller;
+    
+    [SerializeField] private int forceAmount = 1000;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,12 +24,12 @@ public class Booster : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         print("collide");
+        //Controller = collision.gameObject.GetComponent<CharacterController>();
         Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        //Controller.Move(forceAmount * Time.deltaTime * transform.TransformDirection(Vector3.forward));
         if (rb != null)
         {
-            print("rb found");
-            Vector3 force = transform.forward;
-            rb.AddForce(force*1000);
+            rb.AddForce(transform.forward*forceAmount*Time.deltaTime);
         }
     }
 }
--- GMD_project_code/Assets/scripts/PlayerMovement.cs
+++ GMD_project_code/Assets/scripts/PlayerMovement.cs
@@ -23,10 +23,40 @@ public class PlayerMovement : MonoBehaviour
     
     void Update()
     {
-        /*Vector3 PlayerInput = new Vector3
+        Vector3 PlayerInput = new Vector3
         {
             x = Input.GetAxisRaw("Horizontal"),
-            y = 
-        }*/
+            y = 0f,
+            z = Input.GetAxisRaw("Vertical")
+        };
+
+        if (PlayerInput.magnitude > 1f)
+        {
+            PlayerInput.Normalize();
+        }
+
+        Vector3 MoveVector = transform.TransformDirection(PlayerInput);
+        float CurrentSpeed = Input.GetKey(KeyCode.LeftShift) ? RunSpeed : WalkSpeed;
+
+        CurrentMoveVelocity = Vector3.SmoothDamp(CurrentMoveVelocity, MoveVector * CurrentSpeed, ref MoveDampVelocity, MoveSmoothTime);
+
+        Controller.Move(CurrentMoveVelocity * Time.deltaTime);
+
+        Ray groundCheckRay = new Ray(transform.position, Vector3.down);
+        if (Physics.Raycast(groundCheckRay, 1.1f))
+        {
+            CurrentForceVelocity.y = -2f;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                CurrentForceVelocity.y = JumpStrength;
+            }
+        }
+        else
+        {
+            CurrentForceVelocity.y -= GravityStrength * Time.deltaTime;
+        }
+
+        Controller.Move(CurrentForceVelocity * Time.deltaTime);
     }
 }
